5b743158a37401085c3b4e02c93065e76dd27693
SaloniSS <sxs170015@utdallas.edu>
2019-02-17 10:17:57 -0600

    Update to PHP files

---
+++ app/application/views/text/art01.php
@@ -0,0 +1,3 @@
+<h1>Traditional Art</h1>
+<p>Crow Museum of Asian Art, African American Museum of Dallas, Dutch Art Gallery, Amon Carter Museum of American Art, The UpStairs Gallery</p>
+<img src="<?= asset('img/ResultPics/ART - traditional dma.jpg') ?>">
